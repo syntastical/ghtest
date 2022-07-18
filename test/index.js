4db5ba05d639ace966ff9b86b785e91c2feb667f
Sean Rester <syntastical@users.noreply.github.com>
2022-07-18 14:23:04 -0400

    commit

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import { getInput } from '@actions/core'
 import { getOctokit } from '@actions/github'
 
+console.log(getInput("GITHUB_TOKEN").length)
 const octokit = getOctokit(getInput("GITHUB_TOKEN"));
 const releases = await octokit.rest.repos.listReleases({owner: "replicatedhq", repo: "kots"});
 console.log(releases);
